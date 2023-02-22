c3f051c5a469faf7e4fa144488cc927c1acba74f
Pancho Labrador <plabradorjr@gmail.com>
2023-02-21 20:43:47 -0600

    closing positions successfully

--- program/main.py
+++ program/main.py
@@ -1,4 +1,22 @@
-if __name__ == "__main__":
-    print("hello bot!")
+from constants import ABORT_ALL_POSITIONS
+from func_connections import connect_dydx
+from func_private import abort_all_positions
 
-    # test1
+if __name__ == "__main__":
+
+    # Connect to client
+    try:
+        client = connect_dydx()
+    except Exception as e:
+        print("Connecting to Client...")
+        print("Error connecting to client: ", e)
+        exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all positions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions: ", e)
+            exit(1)
