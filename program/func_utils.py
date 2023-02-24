4f54f12443caab4a22777d4c6dbd61a985c45b9a
Pancho Labrador <plabradorjr@gmail.com>
2023-02-24 10:54:02 -0600

    stage 2 start

--- program/func_utils.py
+++ program/func_utils.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+
 # Format number
+
+
 def format_number(curr_num, match_num):
     """
       Give current number an example of number with decimals desired
@@ -15,3 +19,43 @@ def format_number(curr_num, match_num):
         return curr_num_string
     else:
         return f"{int(curr_num)}"
+
+
+# Format time
+def format_time(timestamp):
+    return timestamp.replace(microsecond=0).isoformat()
+
+# Get ISO Times
+
+
+def get_ISO_times():
+
+    # Get timestamps
+    date_start_0 = datetime.now()
+    date_start_1 = date_start_0 - timedelta(hours=100)
+    date_start_2 = date_start_1 - timedelta(hours=100)
+    date_start_3 = date_start_2 - timedelta(hours=100)
+    date_start_4 = date_start_3 - timedelta(hours=100)
+
+    # Format datetimes
+    times_dict = {
+        "range_1": {
+            "from_iso": format_time(date_start_1),
+            "to_iso": format_time(date_start_0),
+        },
+        "range_2": {
+            "from_iso": format_time(date_start_2),
+            "to_iso": format_time(date_start_1),
+        },
+        "range_3": {
+            "from_iso": format_time(date_start_3),
+            "to_iso": format_time(date_start_2),
+        },
+        "range_4": {
+            "from_iso": format_time(date_start_4),
+            "to_iso": format_time(date_start_3),
+        },
+    }
+
+    # Return result
+    return times_dict
--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
-from constants import ABORT_ALL_POSITIONS
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
 from func_connections import connect_dydx
 from func_private import abort_all_positions
+from func_public import construct_market_prices
 
 if __name__ == "__main__":
 
@@ -19,4 +20,15 @@ if __name__ == "__main__":
             close_orders = abort_all_positions(client)
         except Exception as e:
             print("Error closing all positions: ", e)
+            exit(1)
+
+    # Find Cointegrated Pairs
+    if FIND_COINTEGRATED:
+
+        # Construct Market Prices
+        try:
+            print("Fetching market prices, please allow 3 minutes...")
+            df_market_prices = construct_market_prices(client)
+        except Exception as e:
+            print("Error constructing market prices: ", e)
             exit(1)
